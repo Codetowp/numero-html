86ae4a589309b44f2351b07e831ceb50677a0cb1
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2017-08-16 17:06:21 +0530

    update on 16/08/17

--- js/main.js
+++ js/main.js
@@ -122,13 +122,47 @@ $(window).resize();
 /*creating click event*/
 $(document).ready(function(){
   
+   $("#clients").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 5],
+				        [1400, 5],
+				        [1600, 5]
+				      ],
+  	  });
+
+  
+  
   
   $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
+        singleItem:true,
+		afterInit: makePages,
+        afterUpdate: makePages
         });
+		
+		
+		
+		 function makePages() {
+        $.each(this.owl.userItems, function(i) {
+            $('#testimonial .owl-controls .owl-page').eq(i)
+                .css({
+                    'background': 'url(' + $(this).find('img').attr('src') + ')',
+                    'background-size': 'cover'
+                })
+        });
+    }
   
 });
 
